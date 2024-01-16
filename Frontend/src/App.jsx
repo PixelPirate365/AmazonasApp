f4d708d35b6491561eb935a3ce8f91beea253215
Or Hermoni <128356557+PixelPirate365@users.noreply.github.com>
2024-01-16 16:22:02 +0200

    added footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Title from "./components/Title.jsx";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import HomePage from "./Pages/HomePage";
+import Footer from "./components/Shared/Footer";
 function App() {
-
   return (
-   <div>
-    <Title title = "App"></Title>
-    Hello world!
-   </div>
-  )
+    <BrowserRouter>
+      <div className="d-flex flex-column side-allPage min-width">
+        {/* <Header/> */}
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomePage />}/>
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Title from '../components/Shared/Title'
+// rafce shortcut
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
