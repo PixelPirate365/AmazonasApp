8e15b94764d7143ec07994e234754bdcdc7150f2
Or Hermoni <128356557+PixelPirate365@users.noreply.github.com>
2024-01-23 00:06:49 +0200

    searchbox

--- Frontend/src/components/Shared/SearchBox.jsx
+++ Frontend/src/components/Shared/SearchBox.jsx
@@ -1,13 +1,21 @@
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 const SearchBox = () => {
   return (
-    <Form>
+    <Form className="d-flex me-auto w-50">
       <InputGroup>
-        <FormControl type="text" name="q" id="q" placeholder="Search For Product" aria-describedby="button-search">
-
-        </FormControl>
+        <FormControl
+          type="text"
+          name="q"
+          id="q"
+          placeholder="Search For Product"
+          aria-describedby="button-search"
+        ></FormControl>
+        <Button variant="outline-primary" id="button-search">
+          <i className="fa fa-search"></i>
+        </Button>
       </InputGroup>
     </Form>
   );
