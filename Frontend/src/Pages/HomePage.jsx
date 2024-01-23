f390c15734942a97f050a0a19671ab4491233978
Or Hermoni <128356557+PixelPirate365@users.noreply.github.com>
2024-01-23 03:20:33 +0200

    added loading

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useReducer } from "react";
 import Title from "../components/Shared/Title";
 import { homePageReducer } from "../reducers/homePageReducer";
+import Loading from "../components/Shared/Loading";
 import axios from "axios";
 // rafce shortcut
 const initialState = { loading: true, error: "", data: [] };
 const HomePage = () => {
   const [state, dispatch] = useReducer(homePageReducer, initialState);
+  const { loading, error, data } = state;
   useEffect(() => {
     const getProducts = async () => {
       dispatch({ type: "GET_REQUEST" });
@@ -16,7 +18,7 @@ const HomePage = () => {
         dispatch({ type: "GET_SUCCESS", payload: data });
       } catch (error) {
         console.error("Error fetching data:", error);
-        dispatch({ type: "GET_ERROR", payload: error.message });
+        dispatch({ type: "GET_ERROR", payload: error });
       }
     };
     getProducts();
@@ -25,7 +27,16 @@ const HomePage = () => {
   return (
     <div>
       <Title title="HomePage" />
-      HomePage
+      <div className="backgroundHomePage">
+        <img
+          style={{ width: "100%" }}
+          src="https://m.media-amazon.com/images/I/81d5OrWJAkL._SX3000_.jpg"
+          alt="BackgroundHomePage"
+        />
+      </div>
+      <div className="products">
+        {loading ? <Loading /> : error ? <MessageBox /> : <div>nice</div>}
+      </div>
     </div>
   );
 };
